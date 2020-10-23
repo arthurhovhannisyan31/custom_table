4ed9087ee860102ef669bf8f70d9560d3372849e
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2020-10-23 13:08:56 +0400

    minor corrections

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const favicon = require('express-favicon')
 const path = require('path')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+app.use(favicon(`${__dirname}/dist/favicon.ico`))
 
+app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/*', (_, res) => {
