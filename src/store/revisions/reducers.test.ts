019d9a6de8087a5493620b23faf3892d26e4bdc2
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2020-10-25 19:29:32 +0400

    added async api call to reducer test

--- src/store/revisions/reducers.test.ts
+++ src/store/revisions/reducers.test.ts
@@ -5,6 +5,7 @@ import {
   GET_REVISIONS_REQUEST,
 } from '_/store/revisions/constants'
 import revisionsReducer, { initState } from '_/store/revisions/reducers'
+import { fetchTestData } from '_/store/revisions/queries'
 
 describe('test revisions reducer', () => {
   test(`returns updated state upon receiving an action of type ${GET_REVISIONS_REQUEST}`, () => {
@@ -23,33 +24,23 @@ describe('test revisions reducer', () => {
     }
     expect(expectedState).toEqual(resultingState)
   })
-  test(`returns updated state upon receiving an action of type ${GET_REVISIONS_SUCCESS}`, () => {
+  test(`returns updated state upon receiving an action of type ${GET_REVISIONS_SUCCESS}`, async () => {
+    const props = { limit: 5, offset: 0, sort: {} }
+    const { data, total } = await fetchTestData(props)
     const payload = {
-      limit: 5,
-      offset: 0,
-      total: 1,
-      data: [
-        {
-          revision: 646119,
-          revstmp: '2020-08-25T09:34:32.670+0000',
-          user_id: 136,
-          user_email: 'am+pi@rogii.com',
-          user_name: 'A M',
-          well_id: 'cbaa25da-b044-4afc-b074-d56422330740',
-          well_name: 'well_220-test',
-          well_type: 'laterals',
-          changes_summary: 'DELETE dynamic_logs "Dynamic RHOB"',
-        },
-      ],
+      limit: props.limit,
+      offset: props.offset,
+      total,
+      data,
     }
     const action = { type: GET_REVISIONS_SUCCESS, payload }
     const resultState = revisionsReducer(initState, action)
     const expectedState = {
       loading: false,
       error: false,
-      total: 1,
+      total,
       data: {
-        [`${payload?.offset}-${payload?.limit}`]: payload.data,
+        [`${payload.offset}-${payload.limit}`]: payload.data,
       },
     }
     expect(expectedState).toEqual(resultState)
